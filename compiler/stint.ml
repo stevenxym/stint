2cef558f0d5136fec27f8fcf0efb80eaf0cbc972
Sichang Li <seeking.scli@gmail.com>
2012-12-13 23:30:49 -0500

    finished sast and typecheck

--- compiler/stint.ml
+++ compiler/stint.ml
@@ -1,12 +1,15 @@
-type action = Ast
+type action = Ast | Sast
 
 let _ =
   let action = if Array.length Sys.argv > 1 then
-    List.assoc Sys.argv.(1) [ ("-a", Ast) ]
+    List.assoc Sys.argv.(1) [ ("-a", Ast); ("-s", Sast)]
     else Ast in
   let input = open_in Sys.argv.(2) in
   let lexbuf = Lexing.from_channel input in
   let program = Parser.program Scanner.token lexbuf in
+  let program_t = Typecheck.check_program program in
   match action with
-    Ast -> let listing = Ast.string_of_program program
+      Ast -> let listing = Ast.string_of_program program
            in print_string listing
+    | Sast -> let listing = Sast.string_of_program_t program_t
+    	   in print_string listing
